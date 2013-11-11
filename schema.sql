c78f33bc02da887bc4dde7d5be5f218aaf3e9c7c
Tom Booth <tombooth@gmail.com>
2013-11-11 00:49:03 +0000

    Update field length, 65 was wrong

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE SEQUENCE artworks_id_seq;
 
 CREATE TABLE artworks (
 	id integer NOT NULL PRIMARY KEY default nextval('artworks_id_seq'),
-	user_id char(65) NOT NULL,
+	user_id char(64) NOT NULL,
         pid varchar(10),
 	url varchar(512) NOT NULL,
 	inspiration_url varchar(512) NOT NULL,
